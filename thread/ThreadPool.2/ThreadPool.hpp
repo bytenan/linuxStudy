7100b0b810c934466c8baa435ef38cd4dabaf071
nanshao13 <nanshao13@outlook.com>
2023-05-10 18:47:08 +0800

    完成了C++线程池

--- thread/ThreadPool.2/ThreadPool.hpp
+++ thread/ThreadPool.2/ThreadPool.hpp
@@ -12,7 +12,9 @@
 class ThreadPool
 {
 public:
-    ThreadPool(size_t);
+    ThreadPool(size_t threadNums);
+    template<class Function, class... Args>
+    void enqueue(Function &&f, Args &&...args);
     ~ThreadPool();
 
 private:
@@ -27,7 +29,6 @@ inline ThreadPool::ThreadPool(size_t threadNums)
     : stop_(false)
 {
     for (size_t i = 0; i < threadNums; ++i)
-    {
         workers_.emplace_back(
             [this]
             {
@@ -36,19 +37,36 @@ inline ThreadPool::ThreadPool(size_t threadNums)
                     std::function<void()> task;
                     {
                         std::unique_lock<std::mutex> lock(this->mutex_);
-                        while (this->tasks_.empty())
-                        {
-                            this->condition_.wait(lock);
-                        }
+                        this->condition_.wait(lock, [this]
+                                              { return this->stop_ || !this->tasks_.empty(); });
+                        if (this->stop_ && this->tasks_.empty())
+                            return;
                         task = std::move(this->tasks_.front());
                         this->tasks_.pop();
                     }
                     task();
                 }
             });
+}
+
+template<class Function, class... Args>
+void ThreadPool::enqueue(Function &&f, Args &&...args)
+{
+    auto task = std::bind(std::forward<Function>(f), std::forward<Args>(args)...);
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        tasks_.emplace([task]{task(); });
     }
+    condition_.notify_all();
 }
 
 inline ThreadPool::~ThreadPool()
 {
+    {
+        std::unique_lock<std::mutex> lock(this->mutex_);
+        stop_ = true;
+    }
+    condition_.notify_all();
+    for (std::thread &worker : workers_)
+        worker.join();
 }
--- thread/ThreadPool.2/main.cpp
+++ thread/ThreadPool.2/main.cpp
@@ -1,7 +1,26 @@
 #include "ThreadPool.hpp"
 
+void add(int a, int b, int c)
+{
+    std::cout << "result: " << a + b + c << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50000));
+}
+
+void sleep(int count)
+{
+    std::cout << "sleep start" << std::endl;
+    auto start = std::chrono::system_clock::now();
+    std::this_thread::sleep_for(std::chrono::milliseconds(count));
+    auto end = std::chrono::system_clock::now();
+    std::cout << "sleep end" << std::endl;
+    std::chrono::duration<double> times = end - start;
+    std::cout << "sleep times: " << times.count() << std::endl;
+}
+
 int main()
 {
-    ThreadPool tp();
+    ThreadPool tp(5);
+    tp.enqueue(sleep, 100000);
+    tp.enqueue(add, 20, 30, 40);
     return 0;
 }
